973ade6bdb5ddb58af8a4ed55ca3aa88503d3302
sakuma-s <sakuma.shun.programmer@gmail.com>
2026-07-03 07:50:47 +0900

    feat: ログインユーザーの健康記録のみ表示するよう実装

--- src/main/java/com/github/sakumas/healthtracker/controller/HealthRecordController.java
+++ src/main/java/com/github/sakumas/healthtracker/controller/HealthRecordController.java
@@ -1,8 +1,11 @@
 package com.github.sakumas.healthtracker.controller;
 
 import com.github.sakumas.healthtracker.entity.HealthRecord;
+import com.github.sakumas.healthtracker.entity.User;
+import com.github.sakumas.healthtracker.repository.UserRepository;
 import com.github.sakumas.healthtracker.service.HealthRecordService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,10 +21,16 @@ public class HealthRecordController {
 
     @Autowired
     private HealthRecordService healthRecordService;
+    @Autowired
+    private UserRepository userService;
 
     @GetMapping
     public String list(Model model) {
         // 後でログイン中のユーザーを取得する処理を追加
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        User user = userService.findByUsername(username)
+                .orElseThrow(() -> new RuntimeException("ユーザーが見つかりません"));
+        model.addAttribute("healthRecords", healthRecordService.findByUser(user));
         return "records/list";
     }
 
